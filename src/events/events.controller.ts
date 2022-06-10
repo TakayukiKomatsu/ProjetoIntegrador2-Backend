c0aa1cb3a39dd67c07f3646e4ad46fbbba5726dc
Takayuki Komatsu <komatsutakayuki235@live.com>
2022-06-09 21:11:56 -0300

    feat: add Event

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,23 +1,14 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { EventsService } from './events.service';
-import { CreateEventDto } from './dto/create-event.dto';
-import { UpdateEventDto } from './dto/update-event.dto';
+import { Event, Prisma } from '@prisma/client';
 
 @Controller('events')
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
   @Post()
-  create(@Body() createEventDto: CreateEventDto) {
-    return this.eventsService.create(createEventDto);
+  create(@Body() data: Prisma.EventCreateInput): Promise<Event | null> {
+    return this.eventsService.create(data);
   }
 
   @Get()
@@ -26,17 +17,14 @@ export class EventsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.eventsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
-    return this.eventsService.update(+id, updateEventDto);
+  findOne(
+    @Param('id') id: Prisma.EventWhereUniqueInput,
+  ): Promise<Event | null> {
+    return this.eventsService.findOne(id);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.eventsService.remove(+id);
+  remove(@Param('id') id: Prisma.EventWhereUniqueInput) {
+    return this.eventsService.remove(id);
   }
 }
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,26 +1,63 @@
-import { Injectable } from '@nestjs/common';
-import { CreateEventDto } from './dto/create-event.dto';
-import { UpdateEventDto } from './dto/update-event.dto';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Event, Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class EventsService {
-  create(createEventDto: CreateEventDto) {
-    return 'This action adds a new event';
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: Prisma.EventCreateInput): Promise<Event> {
+    try {
+      return await this.prisma.event.create({ data });
+    } catch (error) {
+      console.error(error);
+      throw new BadRequestException(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all events`;
+  async findAll(): Promise<Event[] | null> {
+    try {
+      return this.prisma.event.findMany();
+    } catch (error) {
+      console.error(error);
+      throw new NotFoundException(error);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} event`;
+  async findOne(eventId: Prisma.EventWhereUniqueInput): Promise<Event | null> {
+    try {
+      return this.prisma.event.findUnique({ where: { id: Number(eventId) } });
+    } catch (error) {
+      console.error(error);
+      throw new NotFoundException(error);
+    }
   }
 
-  update(id: number, updateEventDto: UpdateEventDto) {
-    return `This action updates a #${id} event`;
+  async update(
+    where: Prisma.EventWhereUniqueInput,
+    data: Prisma.EventUpdateInput,
+  ) {
+    try {
+      return this.prisma.event.update({
+        data,
+        where,
+      });
+    } catch (error) {
+      console.log(error);
+      throw new BadRequestException(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} event`;
+  async remove(where: Prisma.EventWhereUniqueInput): Promise<Event> {
+    try {
+      return this.prisma.event.delete({ where });
+    } catch (error) {
+      console.error(error);
+      throw new NotFoundException(error);
+    }
   }
 }
