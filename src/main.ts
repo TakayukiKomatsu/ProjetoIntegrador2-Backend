d1972866dfeabad1b0f426752b60b4b5f5aabc08
Takayuki Komatsu <komatsutakayuki235@live.com>
2022-06-11 14:14:24 -0300

    feat: add swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('[Backend] - Controle de temperatura')
+    .setDescription('Projeto integrador 2')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
+
 bootstrap();
