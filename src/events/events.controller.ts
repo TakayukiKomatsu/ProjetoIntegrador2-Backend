0f3c8e56f632be0bcaedd9444fe0e2582abe62ff
Takayuki Komatsu <komatsutakayuki235@live.com>
2022-06-11 14:14:38 -0300

    feat: add swagger on Events controller

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -9,38 +9,45 @@ import {
 } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { Event, Prisma } from '@prisma/client';
+import { ApiTags, ApiParam, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('Eventos')
 @Controller('events')
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
-  @Post()
-  create(@Body() data: Prisma.EventCreateInput): Promise<Event | null> {
-    return this.eventsService.create(data);
+  @Get()
+  async findAll() {
+    return await this.eventsService.findAll();
   }
 
-  @Get()
-  findAll() {
-    return this.eventsService.findAll();
+  @Post()
+  @ApiBody({ description: 'Dados do evento', required: true, type: Object })
+  async create(@Body() data: Prisma.EventCreateInput): Promise<Event | null> {
+    return await this.eventsService.create(data);
   }
 
   @Get(':id')
-  findOne(
+  @ApiParam({ name: 'id', description: 'Id do evento', required: true })
+  async findOne(
     @Param('id') id: Prisma.EventWhereUniqueInput,
   ): Promise<Event | null> {
-    return this.eventsService.findOne(id);
+    return await this.eventsService.findOne(id);
   }
 
   @Put(':id')
-  update(
+  @ApiParam({ name: 'id', description: 'Id do evento', required: true })
+  @ApiBody({ description: 'Dados do evento', required: true, type: Object })
+  async update(
     @Body() data: Prisma.EventUpdateInput,
     @Param('id') id: Prisma.EventWhereUniqueInput,
   ): Promise<Event | null> {
-    return this.eventsService.update(id, data);
+    return await this.eventsService.update(id, data);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: Prisma.EventWhereUniqueInput) {
-    return this.eventsService.remove(id);
+  @ApiParam({ name: 'id', description: 'Id do evento', required: true })
+  async remove(@Param('id') id: Prisma.EventWhereUniqueInput) {
+    return await this.eventsService.remove(id);
   }
 }
