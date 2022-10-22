9841b559dac9ba7a64847f5387bd96a416f18f0e
Takayuki Komatsu <komatsutakayuki235@live.com>
2022-10-22 21:21:47 +0000

    feat: add mqtt initial settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+import { MqttModule } from 'nest-mqtt';
 import { EventsModule } from './module/events/events.module';
 import { RoomModule } from './module/rooms/rooms.module';
 import { SensorsModule } from './module/sensors/sensors.module';
 import { PrismaService } from './prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EventsModule, RoomModule, SensorsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    EventsModule,
+    RoomModule,
+    SensorsModule,
+    MqttModule.forRootAsync({
+      useFactory: () => ({
+        protocol: 'mqtts',
+        port: Number(process.env.MQTT_PORT),
+        host: process.env.MQTT_HOST,
+        username: process.env.MQTT_USERNAME,
+        password: process.env.MQTT_PASSWORD,
+      }),
+    }),
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
