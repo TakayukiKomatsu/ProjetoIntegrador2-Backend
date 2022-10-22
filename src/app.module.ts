5f8cc2b6e3c8e0f70996d1b82c26131356948fab
Takayuki Komatsu <komatsutakayuki235@live.com>
2022-10-22 21:05:25 +0000

    chore: remove sensor-data module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { EventsModule } from './module/events/events.module';
 import { RoomModule } from './module/rooms/rooms.module';
 import { SensorsModule } from './module/sensors/sensors.module';
 import { PrismaService } from './prisma/prisma.service';
-import { SensorDataModule } from './module/sensor-data/sensor-data.module';
 
 @Module({
-  imports: [EventsModule, RoomModule, SensorsModule, SensorDataModule],
+  imports: [EventsModule, RoomModule, SensorsModule],
   providers: [PrismaService],
 })
 export class AppModule {}
