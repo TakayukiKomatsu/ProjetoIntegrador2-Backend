cb80df4bcc983be5a72e1d08cdd7a5997c2c49d3
Takayuki Komatsu <komatsutakayuki235@live.com>
2022-10-22 21:04:10 +0000

    feat: init sensorData module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { EventsModule } from './module/events/events.module';
 import { RoomModule } from './module/rooms/rooms.module';
 import { SensorsModule } from './module/sensors/sensors.module';
 import { PrismaService } from './prisma/prisma.service';
+import { SensorDataModule } from './module/sensor-data/sensor-data.module';
 
 @Module({
-  imports: [EventsModule, RoomModule, SensorsModule],
+  imports: [EventsModule, RoomModule, SensorsModule, SensorDataModule],
   providers: [PrismaService],
 })
 export class AppModule {}
