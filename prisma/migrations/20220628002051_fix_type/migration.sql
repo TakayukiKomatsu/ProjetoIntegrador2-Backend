f0c94a97cf044e5d8f9f046684789a84e12b837c
Takayuki Komatsu <komatsutakayuki235@live.com>
2022-11-27 20:53:15 -0300

    chore: group migrations

--- prisma/migrations/20220628002051_fix_type/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Event" ALTER COLUMN "qtdePessoas" SET DATA TYPE TEXT;
--- prisma/migrations/20220628013446_fix_sensors/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Sensor" ALTER COLUMN "leitura" SET DATA TYPE TEXT,
-ALTER COLUMN "interLeitura" SET DATA TYPE TEXT;
