069082ede82c2e18d2ec0665c2c948319d0f50cc
Takayuki Komatsu <komatsutakayuki235@live.com>
2023-04-15 13:24:45 -0300

    feat: add manual sensorData input endpoint and filter

--- src/module/sensorData/sensorData.controller.ts
+++ src/module/sensorData/sensorData.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { SensorDataService } from './sensorData.service';
 import { Prisma, SensorData } from '@prisma/client';
-import { ApiParam, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('sensorData')
 @Controller('sensorData')
@@ -11,6 +11,34 @@ export class SensorDataController {
   @Get()
   async findAll() {
     return await this.sensorDataService.findAll();
+  }
+
+  @Get('filter')
+  @ApiQuery({
+    name: 'type',
+    description: 'Tipo do sensor',
+    required: true,
+    enum: ['manual', 'automatic'],
+  })
+  @ApiQuery({
+    name: 'topic',
+    description: 'Tópico do sensor - MQTT',
+    required: false,
+    type: String,
+  })
+  async findAllManual(
+    @Query('type') type: 'manual' | 'automatic',
+    @Query('topic') topic: string,
+  ): Promise<SensorData[] | null> {
+    return await this.sensorDataService.findByType(type, topic);
+  }
+
+  @Post()
+  @ApiBody({ description: 'Dados do sensor', required: true, type: Object })
+  async createRow(
+    @Body() data: Prisma.SensorDataCreateInput,
+  ): Promise<SensorData | null> {
+    return await this.sensorDataService.create(data);
   }
 
   @Get(':id')
--- src/module/sensorData/sensorData.service.ts
+++ src/module/sensorData/sensorData.service.ts
@@ -15,6 +15,17 @@ export class SensorDataService {
     }
   }
 
+  async create(data: Prisma.SensorDataCreateInput): Promise<SensorData | null> {
+    try {
+      return await this.prisma.sensorData.create({
+        data,
+      });
+    } catch (error) {
+      console.error(error);
+      throw new NotFoundException(error);
+    }
+  }
+
   async findOne(
     sensorId: Prisma.SensorDataWhereUniqueInput,
   ): Promise<SensorData[] | null> {
@@ -22,6 +33,38 @@ export class SensorDataService {
       return this.prisma.sensorData.findMany({
         where: {
           sensorId: Number(sensorId),
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      throw new NotFoundException(error);
+    }
+  }
+
+  async findByType(
+    type: 'manual' | 'automatic',
+    topic?: string,
+  ): Promise<SensorData[] | null> {
+    try {
+      const filteredSensor = await this.prisma.sensor.findMany({
+        where: { type },
+        select: { id: true },
+      });
+
+      if (!topic) {
+        return await this.prisma.sensorData.findMany({
+          where: {
+            sensorId: { in: filteredSensor.map((sensor) => sensor.id) },
+          },
+        });
+      }
+      return await this.prisma.sensorData.findMany({
+        where: {
+          sensorId: { in: filteredSensor.map((sensor) => sensor.id) },
+          topic: topic,
+        },
+        orderBy: {
+          id: 'asc',
         },
       });
     } catch (error) {
